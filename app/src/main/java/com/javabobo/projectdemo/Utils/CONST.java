c72f1b14460e7235e5772313f6614307e593413b
luis ernesto <ellokodeluis@gmail.com>
2018-08-31 18:17:38 +0100

    Update const

--- app/src/main/java/com/javabobo/projectdemo/Utils/CONST.java
+++ app/src/main/java/com/javabobo/projectdemo/Utils/CONST.java
@@ -9,9 +9,9 @@ public class CONST {
     public static String URL_INSERT_USER = "http://byebox.es/hackathon/insert/insert_user.php";
     public static String URL_INSERT_TASK = "http://byebox.es/hackathon/insert/insert_task.php";
     public static String URL_UPDATE_TASK = "http://byebox.es/hackathon/update/update_task.php";
-    public static  String URL_IMG = "http://byebox.es/hackathon/content/";
-    public static String URL_GET_IMG_GALERY = "http://byebox.es/hackathon/request/get_all_pictures.php";
-    public static String URL_REMOVE_IMG_GALERY = "http://byebox.es/hackathon/remove/remove_image.php";
-    public static String URL_INSERT_IMG_GALERY = "http://byebox.es/hackathon/insert/insert_picture.php";
-    public static String URL_GET_TASKS = "http://byebox.es/hackathon/request/get_all_tasks.php";
+    public static  String URL_IMG = "[URL_WEBSERVICE]/content/";
+    public static String URL_GET_IMG_GALERY = "[URL_WEBSERVICE]/request/get_all_pictures.php";
+    public static String URL_REMOVE_IMG_GALERY = "[URL_WEBSERVICE]/remove/remove_image.php";
+    public static String URL_INSERT_IMG_GALERY = "[URL_WEBSERVICE]/insert/insert_picture.php";
+    public static String URL_GET_TASKS = "[URL_WEBSERVICE]/get_all_tasks.php";
 }
